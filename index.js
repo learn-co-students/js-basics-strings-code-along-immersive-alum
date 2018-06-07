904041a1344d53a103ed2ce22215c70cf753c858
thuyanduong-flatiron <thuyan.duong@flatironschool.com>
2018-06-07 19:29:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = "Ann";
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toLocaleUpperCase()}!`;
+let shortGreeting = `Welcome, ${currentUser[0]}!`;
